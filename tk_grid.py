4b436c620ba53cf717736272ca8b6707b413bcde
chihyeonWON <heungeob1003@gamil.com>
2022-02-01 15:45:11 +0900

    grid의 column과 row에 0 값 넣기

--- tk_grid.py
+++ tk_grid.py
@@ -1,8 +1,11 @@
 from tkinter import *  # tkinter에서 모든 함수를 들고 옵니다.
 win = Tk()
 win.geometry("400x200")
-win.title("pack")
+win.title("grid")
 win.option_add("*Font", "궁서 20")
 
+btn1 = Button(win)
+btn1.config(text="(0,0)")
+btn1.grid(column=0, row=0)
 
 win.mainloop()
