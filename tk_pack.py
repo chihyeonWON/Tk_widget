447891c732a8efb3b01dd15a679f87a636ac8cee
chihyeonWON <heungeob1003@gamil.com>
2022-02-01 15:40:26 +0900

    pady 옵션으로 버튼 간에 간격 지정

--- tk_pack.py
+++ tk_pack.py
@@ -4,5 +4,12 @@ win.geometry("400x200")
 win.title("pack")
 win.option_add("*Font", "궁서 20")
 
+btn = Button(win)
+btn.config(text="버튼")
+btn.pack()
+
+btn1 = Button(win)
+btn1.config(text="버튼1")
+btn1.pack(pady=40)
 
 win.mainloop()
