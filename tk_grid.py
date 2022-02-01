6b081fb564d75b2f25d9d31a9502faf636798ae6
chihyeonWON <heungeob1003@gamil.com>
2022-02-01 16:07:47 +0900

    grid의 column, row, span 옵션 사용하기

--- tk_grid.py
+++ tk_grid.py
@@ -8,4 +8,15 @@ btn1 = Button(win)
 btn1.config(text="(0,0)")
 btn1.grid(column=0, row=0)
 
+btn2 = Button(win)
+btn2.config(text="(1,0)")
+btn2.grid(column=2, row=0)
+
+btn3 = Button(win)
+btn3.config(text="(0,3)")
+btn3.grid(column=0, row=3)
+
+btn4 = Button(win)
+btn4.config(text="셀병합")
+btn4.grid(column=0, row=0, columnspan=1)
 win.mainloop()
